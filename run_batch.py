64f0da372a13d51a13fd7407d9c60ed8f2a5bfa2
Otamio <ca.john.wang@gmail.com>
2021-12-29 01:27:16 -0800

    add command for running batches

---
+++ run_batch.py
@@ -0,0 +1,19 @@
+import argparse
+import subprocess
+
+
+parser = argparse.ArgumentParser(
+    description="Running Machine"
+)
+parser.add_argument('--dataset', default='fb15k237', help='Please provide a dataset path')
+parser.add_argument('--gpu', default='0', help='Please provide a gpu to assign the task')
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    dataset = args.dataset
+    gpu = args.gpu
+    subprocess.run(f"python run.py --dataset {dataset} --gpu {gpu} --model distmult")
+    subprocess.run(f"python run.py --dataset {dataset} --gpu {gpu} --model complex")
+    subprocess.run(f"python run.py --dataset {dataset} --gpu {gpu} --model conve")
+    subprocess.run(f"python run.py --dataset {dataset} --gpu {gpu} --model tucker")
