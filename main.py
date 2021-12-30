51805cb9d42e2f44e4bf4c1201e2e7397224c2b1
Otamio <ca.john.wang@gmail.com>
2021-12-29 18:34:52 -0800

    fix save model logical error

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class Experiment:
         logging.getLogger('').addHandler(console)
         # If need checkpoint, save it
         if save_best:
-            self.target = f"{args.output}/{dataset}_{model}"
+            self.target = f"{args.output}/{dataset}"
             try:
                 os.mkdir(self.target)
             except FileExistsError:
@@ -199,7 +199,7 @@ class Experiment:
 
                     patience = patience - 1 if mrrs[-1] != max(mrrs) else args.patience
                     if save_best and mrrs[-1] == max(mrrs):
-                        torch.save(model, f"{self.target}/best.model")
+                        torch.save(model, f"{self.target}/{args.model}.model")
                     if use_stopper and patience <= 0:
                         logging.info(f"Early stop since no further improvement is made on the validation set")
                         break
